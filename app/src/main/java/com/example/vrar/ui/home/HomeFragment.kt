3e66d7d5a7861a05a9d5c72b122375b4eef9cb06
John Doe <funkdarya@gmail.com>
2019-10-14 20:59:35 +0700

    Add leyron

--- app/src/main/java/com/example/vrar/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/vrar/ui/home/HomeFragment.kt
@@ -1,14 +1,17 @@
 package com.example.vrar.ui.home
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.VideoView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.vrar.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -26,6 +29,9 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(this, Observer {
             textView.text = it
         })
+
+        //idos.setVideoURI(Uri.parse("https://www.youtube.com/watch?v=wn7mLsnsXe4"))
+
         return root
     }
 }
