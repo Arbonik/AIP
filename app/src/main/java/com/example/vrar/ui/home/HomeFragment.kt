29a6b11bdc2bbc786caa22b995774c4188835ee0
John Doe <funkdarya@gmail.com>
2019-10-23 13:09:38 +0700

    Add leyron

--- app/src/main/java/com/example/vrar/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/vrar/ui/home/HomeFragment.kt
@@ -1,17 +1,14 @@
 package com.example.vrar.ui.home
 
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.VideoView
+import android.webkit.WebView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.vrar.R
-import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -22,12 +19,13 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        homeViewModel =  ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
+       val webViewer: WebView = root.findViewById(R.id.webViewer)
+      //  WebView.
+       //
         homeViewModel.text.observe(this, Observer {
-            textView.text = it
+            webViewer.loadUrl("altair/index.html")
         })
 
         //idos.setVideoURI(Uri.parse("https://www.youtube.com/watch?v=wn7mLsnsXe4"))
--- app/src/main/java/com/example/vrar/ui/share/ShareFragment.kt
+++ app/src/main/java/com/example/vrar/ui/share/ShareFragment.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.webkit.WebView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -22,9 +22,10 @@ class ShareFragment : Fragment() {
         shareViewModel =
             ViewModelProviders.of(this).get(ShareViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_share, container, false)
-        val textView: TextView = root.findViewById(R.id.text_share)
+
+
         shareViewModel.text.observe(this, Observer {
-            textView.text = it
+
         })
         return root
     }
