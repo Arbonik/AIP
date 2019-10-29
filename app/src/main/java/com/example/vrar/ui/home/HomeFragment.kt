6d1cc6c580a62b4dd5830a870ca19c43a0c77940
Arbonik <zmear@mail.ru>
2019-10-29 19:13:32 +0700

    up

--- app/src/main/java/com/example/vrar/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/vrar/ui/home/HomeFragment.kt
@@ -25,16 +25,14 @@ class HomeFragment : Fragment() {
         homeViewModel =  ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
 
-        //val webViewer: WebView = root.findViewById(R.id.webViewer)
-       // webViewer.settings.javaScriptEnabled = true
+       // val webViewer: WebView = root.findViewById(R.id.webViewer)
+        //webViewer.settings.javaScriptEnabled = true
        // webViewer.settings.allowFileAccessFromFileURLs = true
 
 
        homeViewModel.text.observe(this, Observer {
 
             //webViewer.loadUrl("file:///android_asset/altair/index.html")
-
-
 
         })
         return root
